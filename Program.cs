723b006a93f1f08bc69e6bf2717f321406552aa6
Tiz <dannilo@bk.ru>
2025-01-16 16:05:56 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Praktika_18_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
